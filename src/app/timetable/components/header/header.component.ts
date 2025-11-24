b2d575913fdf54f18cbf97ffdfcf2dad70a256a9
PrialaRadu <priala.radu@gmail.com>
2025-11-24 09:20:03 +0200

    Implementat design pt filtrarea pe Cursuri, Seminarii si Laburi

--- src/app/timetable/components/header/header.component.ts
+++ src/app/timetable/components/header/header.component.ts
@@ -12,25 +12,33 @@ export class HeaderComponent {
 
   @Input() groups!: number[];
   @Input() selectedGroup!: number;
-
   @Input() specializations!: string[];
   @Input() selectedSpecialization!: string;
-
   @Input() selectedWeek!: 1 | 2;
 
+  // Output-uri existente...
   @Output() groupChange = new EventEmitter<number>();
   @Output() specializationChange = new EventEmitter<string>();
   @Output() weekChange = new EventEmitter<1 | 2>();
 
-  selectGroup(event: any) {
-    this.groupChange.emit(Number(event.target.value));
+  // NOUL OUTPUT pentru filtre
+  @Output() filtersChange = new EventEmitter<{curs: boolean, sem: boolean, lab: boolean}>();
+
+  // Starea filtrelor (toate true by default)
+  filters = {
+    curs: true,
+    sem: true,
+    lab: true
+  };
+
+  // Logica de toggle
+  toggleFilter(type: 'curs' | 'sem' | 'lab') {
+    this.filters[type] = !this.filters[type];
+    this.filtersChange.emit(this.filters);
   }
 
-  selectSpecialization(event: any) {
-    this.specializationChange.emit(event.target.value);
-  }
-
-  selectWeek(w: 1 | 2) {
-    this.weekChange.emit(w);
-  }
+  // ... restul funcțiilor (selectGroup, etc.)
+  selectGroup(event: any) { this.groupChange.emit(Number(event.target.value)); }
+  selectSpecialization(event: any) { this.specializationChange.emit(event.target.value); }
+  selectWeek(w: 1 | 2) { this.weekChange.emit(w); }
 }
